9a079ca00d23cd8e4cfa53ae6372c8cd1ed336de
Develing1991 <completed0728@gmail.com>
2023-03-13 02:35:57 +0900

    scss test

--- src/layouts/headers/TheHeader.vue
+++ src/layouts/headers/TheHeader.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts"></script>
 <template lang="">
-  <div>header</div>
+  <header>
+    <h1>
+      <a href="">logo</a>
+    </h1>
+    <ul>
+      <li>
+        <a href="">menu1</a>
+      </li>
+    </ul>
+  </header>
 </template>
 
-<style lang=""></style>
+<style lang="scss" scoped>
+header {
+  width: 100px;
+}
+h1 {
+  a {
+    color: red;
+  }
+}
+</style>
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,6 +5,12 @@ const DefaultComponent = defineComponent({
   template: '<template></template>'
 })
 
+const styles = {
+  maxWidth: '1024px',
+  margin: '0 auto',
+  backgroundColor: '#e8d7b7'
+}
+
 export function createComponent(name: string, ...components: any[]) {
   const TheHeader: DefineComponent = defineAsyncComponent(components[0]) ?? DefaultComponent
   const TheMain: DefineComponent = defineAsyncComponent(components[1]) ?? DefaultComponent
@@ -12,12 +18,24 @@ export function createComponent(name: string, ...components: any[]) {
   // return h(Fragment, null, [TheHeaderVue, TheMainVue, TheFooterVue])
 
   return defineComponent({
+    conputed() {
+      // index: {
+      //   return `
+      //   .index {
+      //     background-color: red;
+      //     color: white;
+      //   }
+      // `
+      // }
+    },
     render() {
       return (
         <Fragment>
-          <TheHeader></TheHeader>
-          <TheMain></TheMain>
-          <TheFooter></TheFooter>
+          <div style={styles}>
+            <TheHeader></TheHeader>
+            <TheMain></TheMain>
+            <TheFooter></TheFooter>
+          </div>
         </Fragment>
       )
     }
